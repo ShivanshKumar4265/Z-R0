df033b16640e3f5b6bf0ffc5e78084ca16a5be5a
Shivansh Kumar <codes.stalker@gmail.com>
2023-03-17 21:49:41 +0530

    Added Create note activity

--- ScribBox/app/src/main/java/com/example/scribbox/CreateNotesActivity.java
+++ ScribBox/app/src/main/java/com/example/scribbox/CreateNotesActivity.java
@@ -3,13 +3,30 @@ package com.example.scribbox;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class CreateNotesActivity extends AppCompatActivity {
+
+
+    private EditText CreateTitleForNotes,CreateContentOfNotes;
+    private FloatingActionButton SaveNotes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_notes);
+        /**
+         * Finding all the views
+         */
+        CreateTitleForNotes = findViewById(R.id.createTitleForNotes);
+        CreateContentOfNotes = findViewById(R.id.createcontentofnote);
+        SaveNotes = findViewById(R.id.saveNotes);
+        /**
+         * Hiding  the action bar
+         */
+        getSupportActionBar().hide();
 
 
     }
